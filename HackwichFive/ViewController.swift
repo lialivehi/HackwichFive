81fff615cac727b99051b8601346fa3dd4dc8017
lialivehi <111819496+lialivehi@users.noreply.github.com>
2022-09-29 13:33:57 -1000

    Completed problem set one. Changed an item in my array

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -18,13 +18,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var buttonLabel: UIButton!
     
-    
     //Part 6: declare int variable and array variable
   var currentIndex = 0
-  var favoriteFoodsArray = ["Tacos","Poke", "Platanitos", "Curry", "Chocolate" ]
-    
-    
-
+  var favoriteFoodsArray = ["Tacos","Poke", "Platanitos", "Curry", "Rocky Road Ice Cream" ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,17 +34,25 @@ class ViewController: UIViewController {
 //Part 8:
 
     @IBAction func buttonPressed(_ sender: Any) {
-    
+   
     if self.currentIndex<self.favoriteFoodsArray.count
+        ///this starts the conditional statement that "if the current index is less than the count of favorite foods array, it will run the if, and when it is not it will run the else" . It basically sets the amount of items the button will sort through.
     {
     self.bottomLabel.text = favoriteFoodsArray[currentIndex]
+        //this sets the bottom label at the current index which is 0(tacos).
+        
         buttonLabel.setTitle("next", for: UIControl.State.normal)
+        //this line sets the label of the buttons normal state to to the string "next"
+       
         currentIndex += 1
+        //this line adds one to the current index every time the button is pressed so it displays the next item in the array.
     }
     else
     {
     print("button has been disabled")
-        (buttonLabel!).isEnabled = false 
+        //this prints on the console "button has been disabled"
+        (buttonLabel!).isEnabled = false
+        //this statement disbales the next button.
     }
     
     
